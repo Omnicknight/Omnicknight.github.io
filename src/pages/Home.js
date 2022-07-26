c0707f74f2af45e021ff0ec48da0f047fe84249f
Omnicknight <57796930+Omnicknight@users.noreply.github.com>
2022-07-26 23:57:31 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -44,22 +44,23 @@ const SearhForm = () => {
                                 <input {
                                            ...register(
                                                "inputVin", {
-                                                   required: "Поле обязательно для заполнения",
+                                                   required: "Required field",
                                                    maxLength: {
                                                        value: 17,
-                                                       message: "Не больше 17 символов",
+                                                       message: "No more than 17 characters",
                                                    }
                                                }
                                            )
                                        } value={inputOne}
+                                       placeholder="enter your VIN"
                                        onChange={(event) => {
                                            handleChange(event.target.value);
                                        }}/>
+                                <div className="error-form">
+                                    {errors?.inputVin && <p>{errors?.inputVin?.message || "Error!"}</p>}
+                                </div>
                                 <button type="submit" value="Отправить">Search</button>
                             </form>
-                            <div className="error-form">
-                                {errors?.inputVin && <p>{errors?.inputVin?.message || "Error!"}</p>}
-                            </div>
                         </div>
                     </div>
                     <div className="search-wrap__history">
@@ -86,13 +87,15 @@ const SearhForm = () => {
                     </div>
                 </div>
                 <div className="vin-params">
-                    {(Object.keys(resultArr).map(function(key) {
-                                return (
-                                    <div>
-                                        <p>{key}:</p><span>{this[key]}</span>
-                                    </div>
-                                )
+                    <div className="">
+                        {(Object.keys(resultArr).map(function(key, i) {
+                            return (
+                                <div>
+                                    <p>{i+1}) {key}:</p><span>{this[key]}</span>
+                                </div>
+                            )
                         }, resultArr))}
+                    </div>
                 </div>
             </div>
         </div>
